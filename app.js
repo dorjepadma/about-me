db2c7b58d0a22a30fd7222d2936de7bd6196fb2e
dorjepadma <dorjepadma@gmail.com>
2020-01-14 11:17:29 -0800

    test done

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 
     //event goes here
-    import isYes from './isYes.js';
+import isYes from './isYes.js';
     
-    const quizButton = document.getElementById('quizButton');
-    const Results = document.getElementById('Results');
+const quizButton = document.getElementById('quizButton');
+const results = document.getElementById('Results');
 
-    quizButton.addEventListener('click', () =>{
-     console.log  ('button works');
-     const name = prompt ('What\'s your mane mortal?');
-     const confirmation = confirm(`${name} + are you sure you would like to run the quiz guantlet?`);
-     if (confirmation === false) return;
+quizButton.addEventListener('click', () => {
+    console.log  ('button works');
+    const name = prompt ('What\'s your mane mortal?');
+    const confirmation = confirm(`${name} + are you sure you would like to run the quiz gauntlet?`);
+    if (confirmation === false) return;
 
-     const answer1 = prompt('Question 1 is yes');
-     const answer2 = prompt('Question 2 is no');
-     const answer3 = prompt('Question 3 is yes');
-     
-     Let count = 0;
-     if (isYes (answer1) += 1);
-     if (!isYes (answer2) += 1);
-     if (isYes (answer3) += 1);
-     total.textContent = `${name}, you got {count} right! Well Done!`
+    const answer1 = prompt('Are there stones in Boulder?');
+    const answer2 = prompt('Would dorje drink tea before coffee?');
+     // why is answer 3 declared but not reading a value
+    const answer3 = prompt('Should Dorje meditate on emptiness?');
 
-    })
+    let count = 0;
+    if (isYes(answer1)) count += 1;
+    if (!isYes(answer2)) count += 1;
+    if (isYes(answer3)) count += 1;
+    total.textContent = `${name}, you got ${count} right! Well Done!`
+
+})
---
+++ isYes.js
@@ -0,0 +1,6 @@
+export default function isYes(str) {
+    if (str.toLowerCase() === 'yes' || str.toLowerCase() === 'y') 
+        return true; 
+    else 
+        return false;
+}
