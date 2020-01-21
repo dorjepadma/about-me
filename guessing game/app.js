c2b6c2112179a024fb62fa6c04839524be5d8e37
dorjepadma <dorjepadma@gmail.com>
2020-01-20 20:12:46 -0800

    sunday night clean up of js

--- guessing game/app.js
+++ guessing game/app.js
@@ -1,4 +1,4 @@
-import numberGuess from './guessing game/numberGuess.js';
+import compareNumbers from './guessing game/numberGuess.js';
 let randomNumber = Math.floor(Math.random()*20);
 
 const input = document.getElementById('guessField');
@@ -7,29 +7,33 @@ const guesses = document.getElementById('guesses');
 const lowOrHi = document.getElementById('lowOrHi');
 const lastResult = document.getElementById('lastResult');
 let guessCount = 0;
-
 submitGuess.addEventListener('click', () => {
 const userGuess = input.value ()
 function compareNumbers(){
     let userGuess = Number(guessField.value);
     if (guessCount === 4)
-
-{guesses.textContent = 'Previous Guesses:'}
+// where does numberGuess go or what does numberGuess replace
+guesses.textContent = previousGuesses;
 guesses.textContent += ueserGuess + '';
 
-if (userGuess === randomNumber) {
+}  if (userGuess === randomNumber) {
     lastResult.textContent = 'Yama says you may pass to the pure realm!';
     lastResult.style.backgroundColor = 'red';
     lowOrHi.textContent = '';
     setGameOver ();
-} else if (guessCount === 4) {
+}   else if (guessCount === 4) {
     lastResult.textConent = 'To the Lower Realms With You!';
-    lowOrHi.textContent = ''
+    lowOrHi.textContent = '';
     setGameOver ();
-} else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'green';
-    if (userGuess > randomNumber) {
-            lowOrHi.textContent = 'You are not focused, your guess is too High!';
-    } else if(userGuess < randmoNumber) {lowOrHi.textContent = 'Distracted Again! your guess is too high'}
+    } 
+    else if 
+    (userGuess > randomNumber);
+    {
+    lowOrHi.textContent = 'You are not focused, your guess is too High!';
+    } 
+    // else if {
+    (userGuess < randmoNumber);
+        lastResult.textContent = 'Wrong!';
+        lastResult.style.backgroundColor = 'green';
+         lowOrHi.textContent = 'Distracted Again! your guess is too high'}
 });
